73ebee0dc95127ee016fe5278ce647665c6568b1
Rodrigo <rodrigo.lopez@udem.edu>
2021-08-24 15:25:49 -0500

    New

---
+++ testing-RStudio using vectors.R
@@ -0,0 +1,6 @@
+# Code for creating vectors using c()function()
+z <- c(1, 2)          # numeric
+z <- c(TRUE, FALSE)   # logical 
+z <- c("x", "y", "z") # character
+z <- 1:10             # integer
+z <- c(1+0i, 2_li)    # complex
